e9d3bf805d2dafd7a66ecebdf29c314e46db57ef
Seanon1 <seangachango@gmail.com>
2025-07-21 09:37:26 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('Hello, World!');
+let firstName = 'John';
+console.log(`My name is ${firstName}.`);
+let message = " My name is " + firstName + ".";
+console.log(message);
+let age = '20';
+
