a4f6198e53db72b500ed6759634a7195a1eaea5b
Seanon1 <seangachango@gmail.com>
2025-07-21 09:42:50 +0300

    combining strings

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ console.log(`My name is ${firstName}.`);
 let message = " My name is " + firstName + ".";
 console.log(message);
 let age = '20';
+console.log(firstName.length);
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
 
+// Numbers
