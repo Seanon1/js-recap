8c3d48812c727ab39f57b6ca534f8e072cfd53c5
Seanon1 <seangachango@gmail.com>
2025-07-21 10:01:17 +0300

    numbers using scripts and booleans

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ console.log(firstName.toLowerCase());
 console.log(firstName.toUpperCase());
 
 // Numbers
+let num = 10;
+console.log(num)
+
+console.log(5 / 0);
+console.log(-5 / 0);
+
+console.log('Hi' / 2);
+console.log(Math.sqrt(-1));
+
+let largeNumber = 887775848857464756577n;
+console.log(largeNumber);
+let lNumber = BigInt("887775848857464756577");
+console.log(lNumber);
+console.log(largeNumber + lNumber);
+console.log(largeNumber * lNumber);
