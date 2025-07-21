a7ae5d915b2dca226ee847e7bddb23c71a88121c
Seanon1 <seangachango@gmail.com>
2025-07-21 10:54:00 +0300

    volume of a cylinder

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ console.log(`My name is ${firstName}.`);
 let message = " My name is " + firstName + ".";
 console.log(message);
 let age = '20';
+let age2 = parseInt(age);
+console.log(age2)
 console.log(firstName.length);
 console.log(firstName.toLowerCase());
 console.log(firstName.toUpperCase());
@@ -24,3 +26,34 @@ let lNumber = BigInt("887775848857464756577");
 console.log(lNumber);
 console.log(largeNumber + lNumber);
 console.log(largeNumber * lNumber);
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+console.log(Math.sqrt(16));
+console.log(Math.floor(16.9));
+console.log(Math.ceil(16.1));
+console.log(Math.round(16.45665656));
+
+let x = 16.45665656;
+let newX = x.toFixed(2);
+console.log(x, newX);
+
+const radius = 5;
+const height = 12;
+
+const volume = Math.PI * (radius ** 2) * height;
+const volume2 = Math.PI * Math.pow(radius, 2) * height;
+
+console.log(volume.toFixed(2));
+console.log(volume2.toFixed(2));
+
+let isLoggedIn = true;
+let hasPermission = false;
+
