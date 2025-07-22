528760c0221cc4771ced3fbc74ff8cc08adb3160
Seanon1 <seangachango@gmail.com>
2025-07-22 13:17:59 +0300

    while loops

--- loops.js
+++ loops.js
@@ -28,9 +28,34 @@
 // }
 
 let seconds = 5;
-while (seconds > 0 ) {
-    console.log('Time left :', seconds);
-    seconds --;
-}
 
-console.log("Time's up!")
+// while (seconds > 0 ) {
+//     console.log('Time left :', seconds);
+//     seconds --;
+// }
+
+console.log("Time's up!");
+
+const timer = setInterval(() => {
+    console.log('Time left:', seconds); 
+    seconds--;
+
+
+  if (seconds === 0) {
+    clearInterval(timer);
+    console.log("Time's up!");
+  }
+}, 1000);
+
+// do while loop
+//  It executes what is within the do block, and then after that it evaluates the while. If 
+// the condition is true, it will execute what is in the do block again. It will continue to 
+// do so until the condition in the while changes to false
+// let correctPass = '12345';
+// let input;
+
+do {
+    input = prompt('Enter your password:' );
+}  while(input !== correctPass){
+}
+console.log('Access granted!')
