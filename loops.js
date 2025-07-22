50537b964ec8a33353493a73ed78ddc06156dc6d
Seanon1 <seangachango@gmail.com>
2025-07-22 12:52:48 +0300

    for loops

---
+++ loops.js
@@ -0,0 +1,16 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Number:", i)
+// }
+
+let cart = [1000, 2500, 1999, 2999]
+let total = 0;
+for (let i = 0; i < cart.length; i++) {
+    total += cart[i]; 
+}
+
+console.log(total);
+
+let laptops = ['Mac', 'HP', 'Lenovo', 'Asus'];
+for (let i = 0; i < laptops.length; i++) {
+    console.log(laptops[i]);
+}
