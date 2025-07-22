9e857e528001b4965cfb22071a79392cb432f2ad
Seanon1 <seangachango@gmail.com>
2025-07-22 13:01:12 +0300

    while loops

--- loops.js
+++ loops.js
@@ -2,15 +2,35 @@
 //     console.log("Number:", i)
 // }
 
-let cart = [1000, 2500, 1999, 2999]
-let total = 0;
-for (let i = 0; i < cart.length; i++) {
-    total += cart[i]; 
+// For loops
+// // loops through a block of code a number of times
+// let cart = [1000, 2500, 1999, 2999]
+// let total = 0;
+// for (let i = 0; i < cart.length; i++) {
+//     total += cart[i]; 
+// }
+
+// console.log(total);
+
+// let laptops = ['Mac', 'HP', 'Lenovo', 'Asus'];
+// for (let i = 0; i < laptops.length; i++) {
+//     console.log(laptops[i]);
+// }
+
+// While loops
+//  A while loop executes a certain block 
+// of code as long as an expression evaluates to true. 
+// let count = 1;
+
+// while (count <= 10) {
+//     console.log(count);
+//     count++
+// }
+
+let seconds = 5;
+while (seconds > 0 ) {
+    console.log('Time left :', seconds);
+    seconds --;
 }
 
-console.log(total);
-
-let laptops = ['Mac', 'HP', 'Lenovo', 'Asus'];
-for (let i = 0; i < laptops.length; i++) {
-    console.log(laptops[i]);
-}
+console.log("Time's up!")
